584185a836d242a6e11d9abdfa544ab7b9cd2a9a
francismgappe <francismathew.gappe-18@cpu.edu.ph>
2022-01-21 18:01:27 +0800

    feat: added implementations for user route

--- services/user/index.js
+++ services/user/index.js
@@ -1,44 +1,73 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const pool = require("../../db");
 
 /**
  * Returns the user list.
- * Observe good security practices such as removing the password field 
+ * Observe good security practices such as removing the password field
  * from the response object.
- * 
+ *
  * If :id is given, the user with the given id will be returned.
  */
-router.get('/', (request, response) => {
-    response.status(500).send({
-        error: 'Pending Implementations'
-    })
-})
+router.get("/", async (request, response) => {
+  const { rows: users } = await pool.query(
+    "SELECT id, first_name, last_name, username FROM users"
+  );
+  response.json(users);
+  //   response.status(500).send({
+  //     error: "Pending Implementations",
+  //   });
+});
 
 /**
  * Returns the user with the given id.
  */
-router.get('/:id', (request, response) => {
-    response.status(500).send({
-        error: 'Pending Implementations'
-    })
-})
+router.get("/:id", async (request, response) => {
+  const { id } = request.params;
+  const {
+    rows: [user],
+  } = await pool.query(
+    "SELECT id, first_name, last_name, username FROM users WHERE id = $1",
+    [id]
+  );
+  response.json(user);
+  //   response.status(500).send({
+  //     error: "Pending Implementations",
+  //   });
+});
 
 /**
  * Registers a new user.
  */
-router.post('/register', (request, response) => {
-    response.status(500).send({
-        error: 'Pending Implementations'
-    })
-})
+router.post("/register", async (request, response) => {
+  const { first_name, last_name, username, password } = request.body;
+  const {
+    rows: [user],
+  } = await pool.query(
+    "INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, crypt($4, gen_salt('bf'))) RETURNING first_name, last_name, username, password",
+    [first_name, last_name, username, password]
+  );
+  response.json(user);
+  //   response.status(500).send({
+  //     error: "Pending Implementations",
+  //   });
+});
 
 /**
  * Authenticates a user.
  */
-router.post('/login', (request, response) => {
-    response.status(500).send({
-        error: 'Pending Implementations'
-    })
-})
+router.post("/login", async (request, response) => {
+  const { username, password } = request.body;
+  const {
+    rows: [user],
+  } = await pool.query(
+    "SELECT id, first_name, last_name, username FROM users WHERE username = $1 AND password = crypt($2, password)",
+    [username, password]
+  );
+  response.json(user);
+  //   response.status(500).send({
+  //     error: "Pending Implementations",
+  //   });
+});
 
-module.exports = router
+module.exports = router;
