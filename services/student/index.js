54a7e942215f605c1240ff8f680acaa6b88aa2ce
francismgappe <francismathew.gappe-18@cpu.edu.ph>
2022-01-22 22:11:58 +0800

    feat: catch errors on request

--- services/student/index.js
+++ services/student/index.js
@@ -5,74 +5,81 @@ const pool = require("../../db");
  * Returns all students in the database.
  */
 router.get("/", async (request, response) => {
-  const { rows: students } = await pool.query("SELECT * FROM students");
-  response.json(students);
-  //   response.status(500).send({
-  //     error: "Pending Implementations",
-  //   });
+  try {
+    const { rows: students } = await pool.query("SELECT * FROM students");
+    response.status(200).json(students);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 /**
  * Returns a single student in the database.
  */
 router.get("/:id", async (request, response) => {
-  const { id } = request.params;
-  const {
-    rows: [student],
-  } = await pool.query("SELECT * FROM students WHERE id = $1", [id]);
-  response.json(student);
-  // response.status(500).send({
-  //   error: "Pending Implementations",
-  // });
+  try {
+    const { id } = request.params;
+    const {
+      rows: [student],
+    } = await pool.query("SELECT * FROM students WHERE id = $1", [id]);
+    response.status(200).json(student);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 /**
  * Creates a student in the database and returns the created student object.
  */
 router.post("/", async (request, response) => {
-  const { first_name, last_name, email, course } = request.body;
-  const {
-    rows: [student],
-  } = await pool.query(
-    "INSERT INTO students (first_name, last_name, email, course) VALUES($1, $2, $3, $4) RETURNING *",
-    [first_name, last_name, email, course]
-  );
-  response.json(student);
-  // response.status(500).send({
-  //   error: "Pending Implementations",
-  // });
+  try {
+    const { first_name, last_name, email, course } = request.body;
+    const {
+      rows: [student],
+    } = await pool.query(
+      "INSERT INTO students (first_name, last_name, email, course) VALUES($1, $2, $3, $4) RETURNING *",
+      [first_name, last_name, email, course]
+    );
+    response.json(student);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 /**
  * Deletes a student from the database and returns the deleted student object.
  */
 router.delete("/:id", async (request, response) => {
-  const { id } = request.params;
-  const {
-    rows: [student],
-  } = await pool.query("DELETE FROM students WHERE id = $1 RETURNING *", [id]);
-  response.json(student);
-  // response.status(500).send({
-  //   error: "Pending Implementations",
-  // });
+  try {
+    const { id } = request.params;
+    const {
+      rows: [student],
+    } = await pool.query("DELETE FROM students WHERE id = $1 RETURNING *", [
+      id,
+    ]);
+    response.status(200).json(student);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 /**
  * Modifies a student in the database and returns the modified student object.
  */
 router.patch("/:id", async (request, response) => {
-  const { id } = request.params;
-  const { first_name, last_name, email, course } = request.body;
-  const {
-    rows: [student],
-  } = await pool.query(
-    "UPDATE students SET first_name = $1, last_name = $2, email = $3, course = $4 WHERE id = $5 RETURNING *",
-    [first_name, last_name, email, course, id]
-  );
-  response.json(student);
-  // response.status(500).send({
-  //   error: "Pending Implementations",
-  // });
+  try {
+    const { id } = request.params;
+    const { first_name, last_name, email, course } = request.body;
+    const {
+      rows: [student],
+    } = await pool.query(
+      "UPDATE students SET first_name = $1, last_name = $2, email = $3, course = $4 WHERE id = $5 RETURNING *",
+      [first_name, last_name, email, course, id]
+    );
+    response.json(student);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 module.exports = router;
--- services/user/index.js
+++ services/user/index.js
@@ -10,88 +10,100 @@ const pool = require("../../db");
  * If :id is given, the user with the given id will be returned.
  */
 router.get("/", async (request, response) => {
-  const { rows: users } = await pool.query(
-    "SELECT id, first_name, last_name, username FROM users"
-  );
-  response.json(users);
-  //   response.status(500).send({
-  //     error: "Pending Implementations",
-  //   });
+  try {
+    const { rows: users } = await pool.query(
+      "SELECT id, first_name, last_name, username FROM users"
+    );
+    response.json(users);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 /**
  * Returns the user with the given id.
  */
 router.get("/:id", async (request, response) => {
-  const { id } = request.params;
-  const {
-    rows: [user],
-  } = await pool.query(
-    "SELECT id, first_name, last_name, username FROM users WHERE id = $1",
-    [id]
-  );
-  response.json(user);
-  //   response.status(500).send({
-  //     error: "Pending Implementations",
-  //   });
+  try {
+    const { id } = request.params;
+    const {
+      rows: [user],
+    } = await pool.query(
+      "SELECT id, first_name, last_name, username FROM users WHERE id = $1",
+      [id]
+    );
+    response.json(user);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 /**
  * Registers a new user.
  */
 router.post("/register", async (request, response) => {
-  const { first_name, last_name, username, password } = request.body;
-  const {
-    rows: [user],
-  } = await pool.query(
-    "INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, crypt($4, gen_salt('bf'))) RETURNING first_name, last_name, username, password",
-    [first_name, last_name, username, password]
-  );
-  response.json(user);
-  //   response.status(500).send({
-  //     error: "Pending Implementations",
-  //   });
+  try {
+    const { first_name, last_name, username, password } = request.body;
+    const {
+      rows: [user],
+    } = await pool.query(
+      "INSERT INTO users (first_name, last_name, username, password) VALUES ($1, $2, $3, crypt($4, gen_salt('bf'))) RETURNING first_name, last_name, username, password",
+      [first_name, last_name, username, password]
+    );
+    response.json(user);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 /**
  * Authenticates a user.
  */
 router.post("/login", async (request, response) => {
-  const { username, password } = request.body;
-  const {
-    rows: [user],
-  } = await pool.query(
-    "SELECT id, first_name, last_name, username FROM users WHERE username = $1 AND password = crypt($2, password)",
-    [username, password]
-  );
-  response.json(user);
-  //   response.status(500).send({
-  //     error: "Pending Implementations",
-  //   });
+  try {
+    const { username, password } = request.body;
+    const {
+      rows: [user],
+    } = await pool.query(
+      "SELECT id, first_name, last_name, username FROM users WHERE username = $1 AND password = crypt($2, password)",
+      [username, password]
+    );
+    response.json(user);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 // removes a user
 router.delete("/:id", async (request, response) => {
-  const { id } = request.params;
-  const {
-    rows: [user],
-  } = await pool.query(
-    "DELETE FROM users WHERE id = $1 RETURNING id, first_name, last_name, username",
-    [id]
-  );
-  response.json(user);
+  try {
+    const { id } = request.params;
+    const {
+      rows: [user],
+    } = await pool.query(
+      "DELETE FROM users WHERE id = $1 RETURNING id, first_name, last_name, username",
+      [id]
+    );
+    response.json(user);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 // update password
 router.patch("/update-password", async (request, response) => {
-  const { username, old_password, new_password } = request.body;
-  const {
-    rows: [user],
-  } = await pool.query(
-    "UPDATE users SET password = crypt($3, gen_salt('bf')) WHERE username = $1 AND password = crypt($2, password) RETURNING id, first_name, last_name, username",
-    [username, old_password, new_password]
-  );
-  response.json(user);
+  try {
+    const { username, old_password, new_password } = request.body;
+    const {
+      rows: [user],
+    } = await pool.query(
+      "UPDATE users SET password = crypt($3, gen_salt('bf')) WHERE username = $1 AND password = crypt($2, password) RETURNING id, first_name, last_name, username",
+      [username, old_password, new_password]
+    );
+    response.json(user);
+  } catch (error) {
+    response.status(400).send({ error });
+  }
 });
 
 module.exports = router;
